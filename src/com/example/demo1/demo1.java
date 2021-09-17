700b7fc562ed8fae92f2d59b8d2905ada6ae7af2
ghatikawaykole <ghatika99@gmail.com>
2021-09-17 18:07:00 +0530

    Added the new branch

--- src/com/example/demo1/demo1.java
+++ src/com/example/demo1/demo1.java
@@ -3,5 +3,7 @@ package com.example.demo1;
 public class demo1 {
     public static void main(String args[]) {
         System.out.println("welcome to the program");
+        System.out.println("welcome to the UC1 program");
     }
+
 }
